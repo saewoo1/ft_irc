ac20c27d7b92657a4569b04583d6f61fb21b4720
saewoo1 <wooozooo1123@gmail.com>
2023-12-20 14:24:10 +0900

    feat: 메인문 작성

--- ft_irc/srcs/main.cpp
+++ ft_irc/srcs/main.cpp
@@ -2,6 +2,9 @@
 
 int main(int ac, char **av) {
     try {
+        /**
+         * 포트 열고, pollfd값 등록하기
+        */
         Server server(ac, av);
         std::cout << server.getPortNumber() << ", " << server.getPassword() << std::endl;
 
@@ -16,6 +19,33 @@ int main(int ac, char **av) {
                     std::cout << "client -> server connect" << std::endl;
                     server.acceptClient();
                     continue;
+                }
+
+                for (int i = 1; i < server.pollfds.size(); i++) {
+                    if (server.pollfds[i].revents == 0) {
+                        continue; // 이벤트 없음
+                    }
+                    if (server.pollfds[i].revents && POLLHUP || server.pollfds[i].revents && POLLERR) {
+                        quiteServer(server, i);
+                        std::cout << "quit client\n";
+                    } else if (server.pollfds[i].revents && POLLIN) {
+                        int fd = server.pollfds[i].fd;
+                        char buffer[512];
+
+                        memset(buffer, 0, sizeof(buffer));
+                        ssize_t recvByte = recv(fd, buffer, sizeof(buffer), 0);
+
+                        if (recvByte < 0) {
+                            throw std::runtime_error("Error: Fail read");
+                        } else {
+                            std::strcat(server.clientBuffer[fd], buffer);
+                            std::string recvStr(server.clientBuffer[fd]);
+
+                            if (recvStr.find("\r\n") == std::string::npos) {
+                                continue;
+                            }
+                        }
+                    }
                 }
             }
         }
