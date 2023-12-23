a47486f3d66d041322834053158895146c8dada8
saewoo1 <wooozooo1123@icloud.com>
2023-12-23 14:55:26 +0900

    feat: 로그인 시 아스키아트 출력

--- ft_irc/srcs/Login.cpp
+++ ft_irc/srcs/Login.cpp
@@ -20,11 +20,58 @@ bool Login::isValidUserInfo() {
 	return true;
 }
 
+void Login::generateLoginPage() {
+	std::ostringstream oss;
+    oss << allUsers.size();
+    std::string num = oss.str();
+
+	std::string name = userInfo.getNickName();
+	std::string reply =
+		":" + userInfo.getHostName() + " 001 " + name + " :Welcome to the Localnet IRC Network\n"
+		":" + userInfo.getHostName() + " 002 " + name + " :running version ft_irc-42\n"
+		":" + userInfo.getHostName() + " 002 " + name + " :Current local user: " + num + "\n"
+		":" + userInfo.getHostName() + " 375 " + name + " :- <" + serverName + "> Message of the day - \n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⣀⣀⣀⣀⡀⠀⠀⠀⢀⣀⣀⡀⠀⣤⠀⠀⣀⣀⣀⣀⡀⠀⣀⣀⣀⡀⢠⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠁⠈⠈⣗⠀⠀⠀⠀⢀⣼⡃⠖⣿⠀⠨⡇⡀⠁⢀⠀⠀⢾⠀⢸⣏⣹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⢐⡇⠀⠀⠀⠘⠉⣠⣩⣅⡛⠀⠈⠉⢹⡏⠉⠁⠀⠙⢚⠚⠁⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠉⠙⠙⠉⠋⠙⠉⠀⠀⠀⠘⠧⣄⡤⠟⠈⠙⠉⠋⠙⠉⠋⠁⠀⢽⢤⢤⢤⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠓⢓⡇⡿⢸⡇⢠⡫⣓⡄⣿⢸⠀⠘⠓⣻⠂⢸⠅⠀⡾⠚⠂⢸⠇⡯⠀⠚⢺⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⣀⡾⠁⡿⢺⡇⢠⠞⢲⢔⡷⣹⠀⣤⠞⠛⢤⢸⡋⠁⡯⠀⠙⢹⡃⡯⠀⢀⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠟⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠘⠉⠀⠀⣟⢸⡇⠈⠓⠚⠁⡿⢸⠀⠀⣶⠀⠀⡈⠂⠀⠙⠉⠉⢸⡅⡯⠀⢨⠅⠀⠀⢀⣀⣠⡤⣤⢤⣤⢤⣄⣄⣠⢔⠋⣨⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠁⠈⠁⠀⠀⠀⠀⠉⠘⠀⠀⠉⠉⠉⠉⠁⠀⠀⠀⠀⢨⡔⢙⠲⣠⢴⢖⣟⢽⢕⣗⣝⢮⡳⣕⢯⡺⣪⢯⡻⡶⣅⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠀⠀⠀⠷⣬⡛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣡⣟⢽⢕⢯⡪⣗⣝⢮⢎⣗⣝⢮⡳⣝⢮⡣⣗⢽⡹⡽⣶⡀⠀⠀⠀⢀⣀⣀⣀⣀⣄⣀⡀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠉⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣄⣀⣀⠀⠀⡸⡧⡳⣝⢮⡳⣝⢮⢮⡳⡽⡜⣎⢧⢯⣪⡳⣝⢮⡳⣝⢞⢮⣻⣀⣖⢯⢏⡯⣝⣝⢮⣳⡫⣿⠂⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠒⠀⠀⠀⠀⠀⣠⡶⣟⢯⣫⣫⣫⡫⣗⢦⢯⡺⣝⢮⡳⣝⢮⡳⣕⢗⣝⢮⡳⣝⢮⡺⣪⢮⡳⣝⢎⡯⣺⢜⢮⡺⣪⣳⡽⡮⡾⣫⣻⣗⢽⡂⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠀⠀⠓⠷⠦⠀⠀⠀⠀⠀⢸⡯⡾⡻⡽⣺⢾⣶⣝⢮⢳⡳⣹⣪⢳⢝⡼⡵⡝⡮⣳⣓⢗⣝⢮⡳⣝⢮⡳⣝⢮⡳⣝⢮⣫⢳⢝⣞⢼⣪⣯⣾⢽⡳⣕⣿⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠀⠐⠶⠄⠀⠀⠀⠀⠀⠀⠘⣽⡪⣟⢾⡻⡻⣝⢮⡳⣝⢮⢧⡳⣝⡵⣟⢮⡫⣞⢵⡪⣗⡳⡳⣝⢞⡧⡯⣺⡪⣞⢮⡳⣕⢯⢳⢕⣗⢵⣣⡳⡳⣝⡾⠁⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠀⠐⠴⢤⣤⡀⠀⠀⠀⠀⠀⠈⠻⢮⣳⣝⡝⡮⣳⣝⠮⣗⢧⢯⢺⣚⢮⡳⣝⢮⡳⣝⢮⡺⣝⢮⡳⣝⢞⢮⡺⣪⡳⣝⣜⢗⠻⠳⠵⠷⠗⠟⠋⠁⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⢀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠋⠋⠉⠀⢸⢯⡺⣕⡯⠞⠓⢯⣮⡳⣹⢼⠵⣝⣮⠗⠛⠙⠛⢧⣯⡺⣺⢪⢮⣫⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠉⡉⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠴⠤⣸⣗⢽⠞⠀⢀⡀⠀⢸⣝⢮⡳⣝⡾⠁⢀⣠⡀⠀⠈⢷⣝⢮⡫⣞⡏⠀⠙⠳⢤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠓⠚⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⡴⣶⡋⡠⠖⠂⣿⢜⡯⠀⠀⠸⠿⠀⢸⡗⣗⣝⢮⡇⠀⠘⠟⠁⠀⢀⣾⡣⣗⢽⡾⠉⠓⠦⣤⣀⠈⠙⣳⣦⣀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⡾⣻⢕⢯⢮⣳⣄⣤⢶⣯⣫⣇⠀⠀⠀⣠⡴⣟⣝⢮⢎⣗⢿⣤⣀⣀⣀⣠⡾⡳⣝⢮⡳⣯⣀⠀⠀⢀⣩⣷⣾⢿⢕⣟⢷⣄⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠤⡴⠶⠔⠀⠀⠀⠀⣠⡾⡯⡳⣝⢮⣫⢳⡳⠏⠁⠀⣼⢧⡳⣝⢗⡟⣟⣝⢮⡳⣪⡳⣝⢮⡣⣗⢽⢝⡽⣝⢮⡫⡮⡳⣝⢎⡯⡻⣿⢿⣟⢽⣪⡳⣝⢼⢕⣟⣷⡀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠦⠤⠤⠀⠀⠀⢠⣾⡻⣝⢮⡫⡮⡳⣕⢯⠎⠀⠀⠀⣿⡪⣞⣮⣷⣽⣮⣮⣷⣝⣞⣺⣪⣳⢝⢮⡳⣝⢞⡼⡵⣝⢭⢏⡮⡳⣝⣽⣯⡿⡮⡳⡵⡝⡮⣫⣳⣵⠟⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⢸⣗⣝⢮⡳⣝⢮⢯⡺⣽⠀⠀⠀⢸⣷⠟⠩⢐⠠⢐⢀⠢⠠⠡⢉⠍⠍⡙⡙⠻⢾⡮⡳⣝⢮⢮⡳⣝⢮⢯⣾⢿⡾⣝⢮⡫⡮⣫⢞⡵⠋⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⢀⣠⠤⠖⡀⠀⠀⠀⢻⣮⡳⣕⢗⣝⢮⣺⡪⣷⢤⢰⡿⠡⠨⢈⢐⢨⣄⠂⡂⠅⠊⠄⡂⢅⠆⡂⠅⢊⠹⣿⢜⡵⡳⣝⢮⣳⣽⣿⡻⣪⢮⡳⣝⣝⣾⠋⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⡠⠚⢉⣠⡄⠀⠀⠀⠙⣷⡳⣝⢮⡳⡵⡝⡮⣫⡟⠡⠨⢈⠐⠄⢺⠯⢐⠠⢁⠅⠅⡂⣿⠅⡂⠌⡐⡐⢘⣿⡪⡯⡮⣷⢟⡵⣗⢽⣪⡳⣝⢼⠞⠁⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⣀⠴⠛⣉⣤⠔⠀⠀⠀⠈⢻⣪⡳⣕⢯⡺⣝⢽⣌⠌⠨⢐⠨⢈⢐⢐⠐⠨⢀⠂⠅⡂⢂⠡⠠⡁⡂⠔⡁⣺⣯⡾⣟⢽⢕⢯⢮⡳⡵⣹⣪⡓⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⢀⣴⠞⠋⠀⣀⠀⠀⠀⠀⠀⠱⣝⢮⡳⣝⢮⣫⢻⣳⣵⣐⣨⠐⡀⠢⠨⠨⢐⠨⢐⠐⡐⠨⢐⠠⢂⢡⣰⣟⢮⡺⣕⣏⢯⢳⡣⡯⡺⡵⣵⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠀⠀⣠⠾⣫⠔⠀⠀⠀⠀⠀⢪⡳⣝⢼⡺⣜⢵⡣⣗⢽⢝⡟⣟⢾⡶⣵⣴⣬⣤⣌⣤⣵⣴⢮⢾⡫⣗⢵⡳⣝⢮⡺⣪⡳⣝⢮⢯⡺⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠠⠞⢩⠞⠁⠀⠀⠀⠀⠀⠀⠀⢻⡮⡳⣝⢮⡳⣝⢮⡳⣝⢮⡳⣝⢮⡳⣕⢗⡵⣫⡳⣕⢗⡽⣕⢯⢮⡳⣝⢮⢳⢝⢮⣺⣪⡳⣣⢯⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠀⠠⠁⣠⡾⢋⡀⠀⠀⠀⠀⠀⠘⣯⣻⢼⢕⣝⢮⡳⡝⡮⡳⣝⢮⡳⡝⡮⡳⣝⢮⣺⡪⣗⣝⢮⡳⡵⣝⢮⡳⣝⣝⣕⢧⡳⣝⢮⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠀⠀⠼⠉⣰⠏⠀⠄⠀⠀⠀⠀⠀⢹⣿⡳⣝⢮⡳⣕⢯⡫⣝⢮⡣⣗⢽⢭⡫⡮⡳⡵⣹⣪⡺⣪⢞⣕⢗⡳⣝⢼⡪⡮⡳⣽⢮⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠀⠀⠀⡘⠁⠀⠁⠄⠀⠀⠀⠀⠀⠀⢻⣿⣪⡳⣝⢮⢳⢝⢮⡳⣝⢮⡳⡳⣝⢮⢯⡺⡵⢵⢝⢮⡳⣕⢯⡺⣕⢗⣝⢮⣫⣿⡽⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⢷⣹⡪⣞⣝⢭⡳⣝⢎⣗⢽⣱⢳⢝⢮⡺⡭⡳⣝⢵⢝⢮⡳⣝⢮⡳⡳⣝⢾⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+	":" + userInfo.getHostName() + " 372 " + name + " :⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⠹⢪⢮⡳⣝⢮⡳⣕⢗⡵⣫⣫⡳⣝⢮⢯⡺⣹⢭⡳⣝⢮⢳⡹⠝⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n";
+	Communicate::sendToClient(userInfo.getFd(), reply);
+}
+
 Login::Login(UserInfo &userInfo, std::map<int, UserInfo> &allUsers, std::string serverName) : userInfo(userInfo), allUsers(allUsers), serverName(serverName) {
 	// 단순 닉네임 변경사항이라면, 접속 성공 띄우지 마셈
 	if (isValidUserInfo() && userInfo.getNick() == false) {
         userInfo.setActive(true); // 유저 등록
-		Communicate::sendToClient(userInfo.getFd(), "접속성공");
+		generateLoginPage();
         return;
 	}
 }
